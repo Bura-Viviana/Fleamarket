da1554287e1d9c270884fc4817c2ce2d7bd7693f
Bura Viviana <viviana_mariam@yahoo.com>
2021-03-26 20:29:48 +0200

    added django

--- users/urls.py
+++ users/urls.py
@@ -2,7 +2,6 @@ from django.urls import path, include
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    #path('login/', auth_views.LoginView.as_view(teampleate_name='login.html')),
-    path('login/', auth_views.LoginView.as_view(template_name='login.html')),
+    path('login/', auth_views.LoginView.as_view(template_name='users/login.html')),
     path('', include('django.contrib.auth.urls')),
 ]
--- users/views.py
+++ users/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
+
+# def login(request):
+#     return render(request, "login.html")
+# aici am incercat sa definesc view-ul
 # Create your views here.
