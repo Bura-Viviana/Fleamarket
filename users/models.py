d253fd5736d27f236c5137278b52b805a46f0470
Bura Viviana <viviana_mariam@yahoo.com>
2021-03-31 16:36:05 +0300

    addresses and phones model

--- users/models.py
+++ users/models.py
@@ -1,7 +1,15 @@
-# from django.db import models
-# from django.contrib.auth import get_user_model
-#
-# AuthUserModel = get_user_model()
+from django.db import models
+from django.contrib.auth import get_user_model
+from fleamarket.models import CustomModel
+
+AuthUserModel = get_user_model()
+
+class PhoneAndAddress(CustomModel):
+    user=models.ForeignKey(AuthUserModel, on_delete=models.CASCADE)
+    city=models.CharField(max_length=30, null=False)
+    street=models.CharField(max_length=30, null=False )
+    namber=models.IntegerField()
+    phone=models.IntegerField()
 #
 # # Create your models here.
 # class Reviews(models.Model):
