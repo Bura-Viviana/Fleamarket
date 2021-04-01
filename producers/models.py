eb9649230f6569eac10393f25752c07f84115d35
Bura Viviana <viviana_mariam@yahoo.com>
2021-04-01 15:44:34 +0300

    products model and product_categories model

--- producers/models.py
+++ producers/models.py
@@ -10,3 +10,16 @@ class Producer(CustomModel):
     user = models.OneToOneField(AuthUserModel, on_delete=models.CASCADE, primary_key=True, related_name='producer')
     picture = models.ImageField(upload_to='producers', default='producers/default.jpg')
     about = models.TextField(max_length=500, default="Utilizatorul nu a adaugat descriere")
+
+
+class ProductCategories(CustomModel):
+    name = models.TextField(max_length=50)
+
+
+class Products(CustomModel):
+    id_category = models.ForeignKey(ProductCategories, on_delete=models.CASCADE)
+    id_owner = models.ForeignKey(AuthUserModel, on_delete=models.CASCADE)
+    name=models.TextField(max_length=50)
+    description=models.TextField(max_length=500)
+    quantity=models.IntegerField(null=False, default=1)
+    price=models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
